121682c6a868c4549e707f7978cd1a12a2b6c610
Matusek <mmatusek@dsa.reldom.tamu.edu>
2022-10-26 13:58:21 -0500

    update for laravel chart

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+
+use App\Models\User;
+use App\Models\Order;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(1000)->create();
+        Order::factory(5000)->create();
     }
 }
